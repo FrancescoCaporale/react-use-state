8dd42e4ab7a081bd10e58e72a890cd10a54b88d5
FrancescoCaporale <frenk.caporale@gmail.com>
2026-01-21 17:49:04 +0100

    Fatta card

--- src/components/MainMain.jsx
+++ src/components/MainMain.jsx
@@ -1,16 +1,24 @@
 import languages from "./data/languages"
+import { useState } from "react"
+
 function MainMain() {
+    const [selectedLanguage, setselectedLanguage] = useState(languages[0]);
     return (
         <main>
             <div className="buttons">
                 {languages.map((lang) => (
-                    <button key={lang.id}>
+                    <button key={lang.id}
+                        onClick={() => setselectedLanguage(lang)} >
                         {lang.title}
                     </button>
-                ))}
+                ))
+                }
+            </div>
+            <div className="card">
+                <h2>{selectedLanguage.title}</h2>
+                <p>{selectedLanguage.description}</p>
             </div>
         </main>
     )
 }
-
 export default MainMain
